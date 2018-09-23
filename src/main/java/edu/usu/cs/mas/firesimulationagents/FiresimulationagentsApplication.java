dea7826e6c71e4f534118aa32b2e0abd17071068
Udara Weerakoon <udaraweerakoon@overstock.com>
2018-09-23 16:39:06 -0600

    Adding the initial code to the project

---
+++ src/main/java/edu/usu/cs/mas/firesimulationagents/FiresimulationagentsApplication.java
@@ -0,0 +1,12 @@
+package edu.usu.cs.mas.firesimulationagents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiresimulationagentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiresimulationagentsApplication.class, args);
+	}
+}
